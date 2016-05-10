a3c0aaebdd233a1f9a18fe9c4772e86042e0e437
MrMesk <mesk.utp@gmail.com>
2016-05-10 18:31:09 +0200

    Dandelions grow according to map completion

---
+++ Assets/Scripts/GrowDandelionInMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowDandelionInMenu : MonoBehaviour
+{
+    public float maxDandelions;
+    public int level;
+	// Use this for initialization
+	void Start ()
+    {
+       // Debug.Log(DataManagement.data.savedDandelions[level]);
+        float scaleX = DataManagement.data.savedDandelions[level] / maxDandelions * 1.5f + 1;
+        Vector3 scale = new Vector3(scaleX, scaleX, scaleX);
+        transform.localScale = scale;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
